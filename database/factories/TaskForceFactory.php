ace340db522c9349fd69fb97d7713d136cb3ad8a
nana <taty.vogel@gmx.ch>
2020-11-30 10:47:45 +0100

    Add TaskForce Model, Controller, Factory and Migration

---
+++ database/factories/TaskForceFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TaskForce;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TaskForceFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = TaskForce::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
