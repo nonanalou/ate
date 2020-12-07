784e3ed0c7b1aadb91342e0e9d6ade6f247e4beb
nana <taty.vogel@gmx.ch>
2020-12-07 10:20:57 +0100

    Add Task-Force to the dropdown navigation

--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -15,6 +15,9 @@
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
+                    <x-jet-nav-link href="{{ route('task-force') }}" :active="request()->routeIs('task-force')">
+                        {{ __('Task-Force') }}
+                    </x-jet-nav-link>
                 </div>
             </div>
 
--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,5 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
-    Route::get('/task-force', [TaskForceController::class, 'index']);
+    Route::get('/task-force', [TaskForceController::class, 'index'])->name(('task-force'));
 });
