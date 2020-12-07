1521e002e4d40936f974643a295a687196faddb2
nana <taty.vogel@gmx.ch>
2020-12-07 09:32:48 +0100

    Add task-force view

--- app/Http/Controllers/TaskForceController.php
+++ app/Http/Controllers/TaskForceController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TaskForce;
 use Illuminate\Http\Request;
 
 class TaskForceController extends Controller
 {
     //
+    public function index()
+    {
+        $taskForces = TaskForce::all();
+        return view('task-force.index', ['taskForces' => $taskForces]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskForceController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+//require user authentication for ll private routes
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+    Route::get('/task-force', [TaskForceController::class, 'index']);
+});
