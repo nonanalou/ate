7c670917240768e5f1fd39605c948f98be2705ac
Henning Dahlheim <hactar@cyberkraft.ch>
2021-01-02 21:08:26 +0100

    Fix register and db seed

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions\Fortify;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -25,10 +26,13 @@ class CreateNewUser implements CreatesNewUsers
             'password' => $this->passwordRules(),
         ])->validate();
 
+        $role = Role::where('name', 'member')->first();
+
         return User::create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
+            'role_id' => $role->id
         ]);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $admin = \App\Models\Role::create([
+            'name' => 'admin'
+        ]);
+
+        \App\Models\Role::create([
+            'name' => 'member'
+        ]);
+
         \App\Models\User::create([
             'name' => 'Test1',
             'email' => 'test1@example.com',
-            'password' => Hash::make('P@ss0rd')
+            'password' => Hash::make('P@ss0rd'),
+            'role_id' => $admin->id
         ]);
 
         \App\Models\Project::factory()->count(4)->create();
